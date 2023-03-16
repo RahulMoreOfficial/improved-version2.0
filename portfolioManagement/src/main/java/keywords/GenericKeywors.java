5d17925416c9d2fc0c7ab6c10c4ab65e2681c0e5
RahulMoreOfficial <more.rahul31@gmail.com>
2023-03-16 15:12:28 +0530

    Add Source of project into Github

---
+++ portfolioManagement/src/main/java/keywords/GenericKeywors.java
@@ -0,0 +1,25 @@
+package keywords;
+
+public class GenericKeywors {
+
+	public void openBrowser(String browserName) {
+		
+	}
+	public void navigate(String url) {
+		
+	}
+	public void click(String Locators) {
+		
+	}
+	public void type(String locators, String text) {
+		
+	}
+	public void select(String locators, String option) {
+		
+	}
+	public String getText() {
+		return null;
+		
+	}
+	
+}
